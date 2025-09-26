cda9501a22f969f44ba5e5e2f378b8bd9cd244f2
KARAN AGARWAL <karanagarwal19112002522@gmail.com>
2025-09-26 11:28:37 +0530

    Iniital commit

---
+++ src/main/java/com/todo/todoapp/TodoappApplication.java
@@ -0,0 +1,18 @@
+
+
+//localhost:8081 to check
+
+package com.todo.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappApplication.class, args);
+	}
+
+}
